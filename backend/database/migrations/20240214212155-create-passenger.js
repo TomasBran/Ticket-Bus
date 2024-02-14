38314bf7076e3b67888c12e3e777738d27c4a88f
Alberto Gomez Juan <alberto.gomez.j@bairesdev.com>
2024-02-14 23:39:22 +0100

    feat(backend): :sparkles: passenger model and migration

---
+++ backend/database/migrations/20240214212155-create-passenger.js
@@ -0,0 +1,46 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Passengers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      dni: {
+        type: Sequelize.STRING(15),
+        allowNull: false,
+        unique: true
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      userClientId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('Passengers');
+  }
+};
---
+++ backend/database/models/passenger.js
@@ -0,0 +1,48 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Passenger extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Passenger.belongsTo(models.User, {
+        foreignKey: 'userClientId',
+        as: 'user',
+        onDelete: 'CASCADE'
+      });
+    }
+  }
+  Passenger.init(
+    {
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      dni: {
+        type: DataTypes.STRING(15),
+        allowNull: false
+      },
+      userClientId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'User',
+          key: 'id'
+        }
+      }
+    },
+    {
+      sequelize,
+      modelName: 'Passenger'
+    }
+  );
+  return Passenger;
+};
