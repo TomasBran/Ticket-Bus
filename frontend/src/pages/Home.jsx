c1bea21bfe4f85efbad814d26dd9c272ff6cf9aa
redmor1 <lightz17@hotmail.com>
2024-02-15 15:25:31 -0300

    feat(frontend): :sparkles: Add component Profits.jsx to page Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import TravelStepsWrapper from '../components/TravelStepsWrapper.jsx';
 import BenefitPictures from '../components/BenefitPictures.jsx';
 import Hero from '../components/Hero.jsx';
+import FormSearchTravels from '../components/FormSearchTravels/components/organisms/FormSearchTravels.jsx';
+import Profits from '../components/Profits.jsx';
 
 function Home() {
   return (
     <main>
       <Hero />
+      <FormSearchTravels />
       <TravelStepsWrapper />
+      <Profits />
       <BenefitPictures />
     </main>
   );
