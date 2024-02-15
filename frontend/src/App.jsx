0813a0d30a1fbca478e3b98a5a4443b5f282a1ed
redmor1 <lightz17@hotmail.com>
2024-02-14 23:32:01 -0300

    feat(frontend): :sparkles: Setup routing and main layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,7 @@
+import { RouterProvider } from 'react-router-dom';
+import router from './router.jsx';
 
 function App() {
-  return (
-    <h1 className='text-3xl font-bold underline dark:text-white'>
-      Hello world!
-    </h1>
-  );
+  return <RouterProvider router={router} />;
 }
 export default App;
