b6a9ac9fd1bdbe6bd84e56555c4ebabaefbc0f77
redmor1 <lightz17@hotmail.com>
2024-02-14 23:34:08 -0300

    feat(frontend): :sparkles: Create component Home.jsx

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import TravelStepsWrapper from '../components/TravelStepsWrapper.jsx';
+import BenefitPictures from '../components/BenefitPictures.jsx';
+import Hero from '../components/Hero.jsx';
+
+function Home() {
+  return (
+    <main>
+      <Hero />
+      <TravelStepsWrapper />
+      <BenefitPictures />
+    </main>
+  );
+}
+
+export default Home;
