e8ade9ade85ca2c067f21c7687e437c25fe1c98b
Camilo Martinez <106632070+CamiloProg@users.noreply.github.com>
2024-02-14 19:39:26 -0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
-import { Hero } from './Hero.jsx';
+
 function App() {
   return (
     <h1 className='text-3xl font-bold underline dark:text-white'>
       Hello world!
     </h1>
   );
-
+}
 export default App;
