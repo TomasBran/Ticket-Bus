7ff64e5c05971d6fb4d89c5c4e65019556316ac6
redmor1 <lightz17@hotmail.com>
2024-02-20 14:44:24 -0300

    refactor(frontend): :art: Create placeholder data and use through components

--- frontend/src/components/PricesCard/PriceList.jsx
+++ frontend/src/components/PricesCard/PriceList.jsx
@@ -1,6 +1,12 @@
 import PriceRow from './PriceRow.jsx';
 import Seat from '../../assets/PricesCard/seat.svg';
 
+// data endpoint
+const data = {
+  price: 8000,
+  seatType: 'Semi Cama'
+};
+
 function PriceList() {
   return (
     <table className='table'>
@@ -9,11 +15,13 @@ function PriceList() {
           <th className='pb-1 font-bold text-xl text-[#1A202C] tracking-tight'>
             Lista de precios
           </th>
-          <th className='pb-7 text-[#A0A0A0] font-medium text-sm'>SEMI CAMA</th>
+          <th className='pb-7 text-[#A0A0A0] font-medium text-sm uppercase'>
+            {data.seatType}
+          </th>
         </tr>
       </thead>
       <tbody>
-        <PriceRow src={Seat} price={8000} />
+        <PriceRow src={Seat} price={data.price} />
       </tbody>
     </table>
   );
