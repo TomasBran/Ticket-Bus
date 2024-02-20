ebf05e5ea50c4b39ecc438c48d2b608a08da0121
Brayan Sanchez <brayan_sanchez99@hotmail.es>
2024-02-19 18:44:06 -0600

    style(frontend): Change in the Hero component
    Change in the Hero component to make it responsive

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -1,16 +1,14 @@
-export const Hero = () => {
+const Hero = () => {
   return (
-    <div className='flex items-center m-8 mr-0'>
-      <div className=' mt-10 text-left p-20 '>
-        <p className='font-semibold my-7'>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit.{' '}
+    <div className='flex flex-col sm:pt-0 md:flex-row items-center m-8 md:mr-0'>
+      <div className='sm:mt-10 text-left p-8 md:p-20'>
+        <p className='font-semibold my-4'>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
         </p>
-        <h1 className='font-semibold  text-5xl my-5'>
-          {' '}
-          LOREM IPSUM DOLOR SIT AMET CONSECTUR{' '}
+        <h1 className='font-semibold text-3xl md:text-5xl my-4'>
+          LOREM IPSUM DOLOR SIT AMET CONSECTUR
         </h1>
         <p>
-          {' '}
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet, vel
           aliquid dolorem veniam consequatur minus voluptate fugit esse
           voluptatibus, reiciendis asperiores autem iste perspiciatis. Provident
@@ -19,7 +17,7 @@ export const Hero = () => {
       </div>
       <div>
         <img
-          className=' rounded-tl-[100px] '
+          className='rounded-tl-[100px] md:rounded-tl-[100px]'
           src='https://www.voyenbus.com/assets/img/veb/veb_empresas_header_02.webp'
           alt=''
         />
@@ -27,4 +25,5 @@ export const Hero = () => {
     </div>
   );
 };
+
 export default Hero;
