b4c27efec85016d0d5d55c00206123c1b57e1f02
redmor1 <lightz17@hotmail.com>
2024-02-21 15:01:21 -0300

    feat(frontend): :sparkles: Implement functionality to disable ContinueButton

--- frontend/src/components/ContinueButton.jsx
+++ frontend/src/components/ContinueButton.jsx
@@ -1,8 +1,15 @@
 import { useNavigate, useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 function ContinueButton() {
   const navigate = useNavigate();
   const location = useLocation();
+
+  // Access the travelSelected state from the Redux store
+  const travelSelected = useSelector((state) => state.travel.travelSelected);
+
+  // Check if the travelSelected object is empty
+  const isTravelSelectedEmpty = Object.keys(travelSelected).length === 0;
 
   //   TODO: agregar mas casos mientras vayamos agregando vistas
   function handleClick() {
@@ -20,8 +27,9 @@ function ContinueButton() {
 
   return (
     <button
-      className='bg-[#D97706] px-5 rounded-[40px] h-11 flex items-center w-40 justify-center font-semibold text-white tracking-tight'
+      className={`bg-[#D97706] px-5 rounded-[40px] h-11 flex items-center w-40 justify-center font-semibold text-white tracking-tight ${isTravelSelectedEmpty ? 'cursor-not-allowed bg-gray-600' : ''}`}
       onClick={() => handleClick()}
+      disabled={isTravelSelectedEmpty}
     >
       Continuar
     </button>
