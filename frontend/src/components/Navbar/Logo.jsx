a5e178f579bbb31276789046c821661c1eff9420
Matias Zarate <lightz17@hotmail.com>
2024-02-21 21:22:21 -0300

    feat(frontend): Add component Navbar to StepsLayout (#88)

--- frontend/src/components/Navbar/Logo.jsx
+++ frontend/src/components/Navbar/Logo.jsx
@@ -1,6 +1,9 @@
 export const Logo = () => {
   return (
-    <a href='/' className='flex-1 font-semibold text-4xl'>
+    <a
+      href='/'
+      className='flex-1 md:font-semibold font-bold text-white md:text-[#1A202C] md:text-4xl'
+    >
       Logo
     </a>
   );
--- frontend/src/layouts/StepsLayout.jsx
+++ frontend/src/layouts/StepsLayout.jsx
@@ -1,10 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import { Steps } from '../components/Steps/Steps.jsx';
 import { TravelHeader } from '../components/TravelHeader/index.jsx';
+import Navbar from '../components/Navbar/Navbar.jsx';
 
 function StepsLayout() {
   return (
     <>
+      {/* TODO: queda un espacio entre md y lg donde falta,  */}
+      <div className='md:hidden'>
+        <Navbar />
+      </div>
       <TravelHeader />
       <Steps />
       <Outlet />
