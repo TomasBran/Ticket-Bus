95e0af8b2d8541ae5e05131886e65bd32f7ce8b0
Tomás Bran <tomasaugusto.bran@hotmail.com>
2024-02-22 13:05:45 -0300

    style(frontend): changed Seats style to be more accurate with Figma's design

--- frontend/src/components/Seats/Seats.jsx
+++ frontend/src/components/Seats/Seats.jsx
@@ -1,6 +1,23 @@
 import { useEffect, useState } from 'react';
 import driver from '../../assets/Seats/driver.svg';
+import wc from '../../assets/Seats/wc.svg';
 import PropTypes from 'prop-types';
+import Toast from './Toast';
+
+const SectionA_SeatsId = [
+  1, 2, 5, 6, 9, 10, 11, 12, 13, 14, 17, 18, 21, 22, 25, 26, 29, 30, 33, 34, 37,
+  38
+];
+
+const SectionB_SeatsId = [3, 4, 7, 8];
+
+const SectionC_SeatsId = [
+  15, 16, 19, 20, 23, 24, 27, 28, 31, 32, 35, 36, 39, 40
+];
+
+const SectionD_SeatsId = [41, 42, 44, 45, 47, 48, 50, 51];
+
+const SectionE_SeatsId = [43, 46, 49, 52];
 
 const Seats = (props) => {
   const [sectionA, setSectionA] = useState([]);
@@ -10,24 +27,26 @@ const Seats = (props) => {
   const [sectionE, setSectionE] = useState([]);
   const [floor, setFloor] = useState('first');
   const [showToast, setShowToast] = useState(false);
+  const [toastTimerId, setToastTimerId] = useState(null);
   const { tickets = 1 } = props;
 
-  const initializeSection = (setSection, seatAmount) => {
+  const initializeSection = (setSection, seatAmount, seatsId, section) => {
     const newArray = Array.from({ length: seatAmount }, (_, index) => ({
-      id: index + 1,
+      section,
+      id: seatsId[index],
       status: 'free'
-      //   status: Math.random() < 0.7 ? 'free' : 'occupied' // ESTO ES PARA TESTEAR FUNCIONALIDAD. DESPUES VENDRA DESDE EL BACK
+      // status: Math.random() < 0.7 ? 'free' : 'occupied' // ESTO ES PARA TESTEAR FUNCIONALIDAD. DESPUES VENDRA DESDE EL BACK
     }));
 
     setSection(newArray);
   };
 
   useEffect(() => {
-    initializeSection(setSectionA, 16);
-    initializeSection(setSectionB, 4);
-    initializeSection(setSectionC, 8);
-    initializeSection(setSectionD, 8);
-    initializeSection(setSectionE, 4);
+    initializeSection(setSectionA, 22, SectionA_SeatsId, 'A');
+    initializeSection(setSectionB, 4, SectionB_SeatsId, 'B');
+    initializeSection(setSectionC, 14, SectionC_SeatsId, 'C');
+    initializeSection(setSectionD, 8, SectionD_SeatsId, 'D');
+    initializeSection(setSectionE, 4, SectionE_SeatsId, 'E');
   }, []);
 
   const toggleFloor = (floor) => {
@@ -54,9 +73,15 @@ const Seats = (props) => {
         return;
       }
       setShowToast(true);
-      setTimeout(() => {
+
+      if (toastTimerId) {
+        clearTimeout(toastTimerId);
+      }
+
+      const newToastTimerId = setTimeout(() => {
         setShowToast(false);
-      }, 3000);
+      }, 2000);
+      setToastTimerId(newToastTimerId);
       return;
     }
 
@@ -73,61 +98,79 @@ const Seats = (props) => {
     });
   };
 
+  const handleToastClose = () => {
+    if (toastTimerId) {
+      clearTimeout(toastTimerId);
+    }
+    setShowToast(false);
+  };
+
+  useEffect(() => {
+    return () => {
+      clearTimeout(toastTimerId);
+    };
+  }, [toastTimerId]);
+
   return (
     <div className='flex flex-col items-center w-full max-w-[350px] justify-start m-2'>
       <p className='text-3xl font-semibold'>Elige tu asiento</p>
-      <div className='flex justify-center gap-8 w-full m-4'>
+      <div className='flex justify-center gap-6 w-full m-4'>
         <button
-          className={`btn ${floor === 'first' ? 'btn-accent' : ''}`}
+          className={`rounded font-semibold transition active:scale-95 px-9 py-3 ${floor === 'first' ? 'bg-green-500 hover:bg-green-400 active:bg-green-300' : 'bg-gray-300 hover:bg-gray-200 active:bg-gray-100'}`}
           onClick={() => toggleFloor(1)}
         >
           Nivel 1
         </button>
         <button
-          className={`btn ${floor === 'second' ? 'btn-accent' : ''}`}
+          className={`rounded font-semibold transition active:scale-95 px-9 py-3 ${floor === 'second' ? 'bg-green-500 hover:bg-green-400 active:bg-green-300' : 'bg-gray-300 hover:bg-gray-200 active:bg-gray-100'}`}
           onClick={() => toggleFloor(2)}
         >
           Nivel 2
         </button>
       </div>
       <p className='mb-4 font-medium text-gray-400 text-sm'>
-        {floor === 'first' ? 'Primer' : 'Segundo'} piso
+        PLANTA {floor === 'first' ? 'BAJA' : 'ALTA'}
       </p>
 
       {floor === 'first' && (
         <div className='w-full flex justify-center items-center flex-col'>
-          <div className='w-5/6 flex justify-start mb-6 px-4'>
-            <img src={driver} alt='driver_logo' />
+          <div className='w-5/6 flex flex-col justify-start mb-6 px-3 gap-6 '>
+            <img src={driver} alt='driver_logo' className='w-10' />
+            <img src={wc} alt='wc_logo' className='w-10' />
           </div>
           <div className='flex w-5/6 items-center justify-between'>
             <div className='grid grid-cols-2 grid-rows-2 gap-4 w-full justify-items-center'>
               {sectionD.map((seat) => (
                 <div
                   key={seat.id}
-                  className={`w-12 h-12 rounded-lg transition ${
+                  className={`flex justify-center items-center w-10 h-10 rounded-lg transition ${
                     seat.status === 'occupied'
-                      ? 'bg-gray-200 cursor-default'
+                      ? 'bg-gray-400 cursor-default text-gray-300 '
                       : seat.status === 'free'
-                        ? 'bg-yellow-400 hover:bg-yellow-300 active:bg-yellow-200 cursor-pointer'
-                        : 'bg-green-300 hover:bg-green-200 active:bg-green-100 cursor-pointer'
+                        ? 'bg-yellow-400 hover:bg-yellow-300 active:bg-yellow-200 cursor-pointer text-white'
+                        : 'bg-orange-500 hover:bg-orange-400 active:bg-orange-300 cursor-pointer text-white'
                   }`}
                   onClick={() => toggleSeat(seat.id, sectionD, setSectionD)}
-                />
+                >
+                  {String(seat.id).padStart(2, '0')}
+                </div>
               ))}
             </div>
             <div className='grid grid-cols-1 grid-rows-4 gap-4 w-full justify-items-end'>
               {sectionE.map((seat) => (
                 <div
                   key={seat.id}
-                  className={`w-12 h-12 rounded-lg transition ${
+                  className={`flex justify-center items-center w-10 h-10 rounded-lg transition ${
                     seat.status === 'occupied'
-                      ? 'bg-gray-200 cursor-default'
+                      ? 'bg-gray-400 cursor-default text-gray-300 '
                       : seat.status === 'free'
-                        ? 'bg-yellow-400 hover:bg-yellow-300 active:bg-yellow-200 cursor-pointer'
-                        : 'bg-green-300 hover:bg-green-200 active:bg-green-100 cursor-pointer'
+                        ? 'bg-yellow-400 hover:bg-yellow-300 active:bg-yellow-200 cursor-pointer text-white'
+                        : 'bg-orange-500 hover:bg-orange-400 active:bg-orange-300 cursor-pointer text-white'
                   }`}
                   onClick={() => toggleSeat(seat.id, sectionE, setSectionE)}
-                />
+                >
+                  {String(seat.id).padStart(2, '0')}
+                </div>
               ))}
             </div>
           </div>
@@ -135,20 +178,22 @@ const Seats = (props) => {
       )}
 
       {floor === 'second' && (
-        <div className='w-full flex justify-center gap-12'>
+        <div className='w-full flex justify-center gap-10'>
           <div className='grid grid-cols-2 grid-rows-8 gap-4 w-3/6 justify-items-center'>
             {sectionA.map((seat) => (
               <div
                 key={seat.id}
-                className={`w-12 h-12 rounded-lg transition ${
+                className={`flex justify-center items-center w-10 h-10 rounded-lg transition ${
                   seat.status === 'occupied'
-                    ? 'bg-gray-200 cursor-default'
+                    ? 'bg-gray-400 cursor-default text-gray-300 '
                     : seat.status === 'free'
-                      ? 'bg-yellow-400 hover:bg-yellow-300 active:bg-yellow-200 cursor-pointer'
-                      : 'bg-green-300 hover:bg-green-200 active:bg-green-100 cursor-pointer'
+                      ? 'bg-yellow-400 hover:bg-yellow-300 active:bg-yellow-200 cursor-pointer text-white'
+                      : 'bg-orange-500 hover:bg-orange-400 active:bg-orange-300 cursor-pointer text-white'
                 }`}
                 onClick={() => toggleSeat(seat.id, sectionA, setSectionA)}
-              />
+              >
+                {String(seat.id).padStart(2, '0')}
+              </div>
             ))}
           </div>
           <div className='flex flex-col w-3/6 items-center justify-between'>
@@ -156,43 +201,46 @@ const Seats = (props) => {
               {sectionB.map((seat) => (
                 <div
                   key={seat.id}
-                  className={`w-12 h-12 rounded-lg transition ${
+                  className={`flex justify-center items-center w-10 h-10 rounded-lg transition ${
                     seat.status === 'occupied'
-                      ? 'bg-gray-200 cursor-default'
+                      ? 'bg-gray-400 cursor-default text-gray-300 '
                       : seat.status === 'free'
-                        ? 'bg-yellow-400 hover:bg-yellow-300 active:bg-yellow-200 cursor-pointer'
-                        : 'bg-green-300 hover:bg-green-200 active:bg-green-100 cursor-pointer'
+                        ? 'bg-yellow-400 hover:bg-yellow-300 active:bg-yellow-200 cursor-pointer text-white'
+                        : 'bg-orange-500 hover:bg-orange-400 active:bg-orange-300 cursor-pointer text-white'
                   }`}
                   onClick={() => toggleSeat(seat.id, sectionB, setSectionB)}
-                />
+                >
+                  {String(seat.id).padStart(2, '0')}
+                </div>
               ))}
             </div>
             <div className='grid grid-cols-2 grid-rows-4 gap-4 w-full justify-items-center'>
               {sectionC.map((seat) => (
                 <div
                   key={seat.id}
-                  className={`w-12 h-12 rounded-lg transition ${
+                  className={`flex justify-center items-center w-10 h-10 rounded-lg transition ${
                     seat.status === 'occupied'
-                      ? 'bg-gray-200 cursor-default'
+                      ? 'bg-gray-400 cursor-default text-gray-300 '
                       : seat.status === 'free'
-                        ? 'bg-yellow-400 hover:bg-yellow-300 active:bg-yellow-200 cursor-pointer'
-                        : 'bg-green-300 hover:bg-green-200 active:bg-green-100 cursor-pointer'
+                        ? 'bg-yellow-400 hover:bg-yellow-300 active:bg-yellow-200 cursor-pointer text-white'
+                        : 'bg-orange-500 hover:bg-orange-400 active:bg-orange-300 cursor-pointer text-white'
                   }`}
                   onClick={() => toggleSeat(seat.id, sectionC, setSectionC)}
-                />
+                >
+                  {String(seat.id).padStart(2, '0')}
+                </div>
               ))}
             </div>
           </div>
         </div>
       )}
 
-      {showToast && (
-        <div className='toast'>
-          <div className='alert alert-warning flex'>
-            <span>Ya seleccionaste tus {tickets} asientos.</span>
-          </div>
-        </div>
-      )}
+      <Toast
+        showToast={showToast}
+        setShowToast={setShowToast}
+        tickets={tickets}
+        handleToastClose={handleToastClose}
+      />
     </div>
   );
 };
