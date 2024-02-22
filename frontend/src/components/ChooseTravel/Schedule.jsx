23f91ea6b13cadf66c9e6c529529fda6be9b04ab
redmor1 <lightz17@hotmail.com>
2024-02-21 22:49:37 -0300

    feat(frontend): :sparkles: Add functionality to switch colors of component Schedule

--- frontend/src/components/ChooseTravel/Schedule.jsx
+++ frontend/src/components/ChooseTravel/Schedule.jsx
@@ -1,10 +1,14 @@
 import './Schedule.css';
 import green from '../../assets/arrow-green.svg';
+import white from '../../assets/arrow-white.svg';
+
 import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import isEqual from 'lodash-es/isEqual';
 
 export default function Schedule({ departure, arrival, origin, destination }) {
   const dispatch = useDispatch();
+  const travelSelected = useSelector((state) => state.travel.travelSelected);
 
   // TODO: Adjust when back-end endpoint is available
   const travel = {
@@ -18,24 +22,29 @@ export default function Schedule({ departure, arrival, origin, destination }) {
     dispatch({ type: 'SET_TRAVEL_SELECTED', payload: travel });
   };
 
+  // Check if the current Schedule component is active
+  const isActive = isEqual(travel, travelSelected); // Use _.isEqual to compare the objects
+
   return (
     // TODO: button encompasando cambia los estilos, son aplicados por defecto a los elementos button
     // tengo que revisar cuales estilos exactamente se aplican y removerlos en Schedule.css
     <button className='daisy-btn-block select-button' onClick={handleSelect}>
-      <div className='bg-[#93E0BB] flex  py-8 px-4 rounded-lg mb-4 pr-12 justify-between w-full flex-col md:flex-row relative'>
+      <div
+        className={`flex py-8 px-4 rounded-lg mb-4 pr-12 justify-between w-full flex-col md:flex-row relative ${isActive ? 'bg-[#93E0BB]' : 'bg-[#DEE0E4]'}`}
+      >
         <div className='flex gap-12 mb-8 md:mb-0 '>
-          <div className='gap-2 flex flex-col'>
-            <p className='font-medium text-lg'>{departure}</p>
-            <p className='text-sm font-normal'> {origin}</p>
+          <div className='gap-2 flex flex-col text-[#486284]'>
+            <p className='font-medium text-lg '>{departure}</p>
+            <p className='text-sm font-normal '> {origin}</p>
           </div>
 
           <div className='flex flex-col items-center gap-1'>
             <p className='font-normal text-xs'>4 hrs 45min</p>
-            <img src={green} alt='' />
+            <img src={isActive ? white : green} alt='' />
           </div>
 
-          <div className='gap-2 flex flex-col'>
-            <p className='font-medivdium text-lg'>{arrival}</p>
+          <div className='gap-2 flex flex-col text-[#486284]'>
+            <p className='font-medium text-lg'>{arrival}</p>
             <p className='text-sm font-normal'>{destination} </p>
           </div>
         </div>
