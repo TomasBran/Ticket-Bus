1a86378e7b05926582cde60e846cdb04baf6f2a7
Alberto Gomez Juan <alberto.gomez.j@bairesdev.com>
2024-02-22 20:00:59 +0100

    feat: :sparkles: API home page

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-  res.send('Hola mundo!');
+  res.render('index', { title: 'Ticket Bus' });
 });
 
 module.exports = router;
