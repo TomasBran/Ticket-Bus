1f774aa2480e515c58c6510e00712f5efbd0d7ad
redmor1 <lightz17@hotmail.com>
2024-02-29 14:00:17 -0300

    feat(frontend): :sparkles: Add link back to Home in page RegisterAlt

--- frontend/src/pages/auth/RegisterAlt.jsx
+++ frontend/src/pages/auth/RegisterAlt.jsx
@@ -39,16 +39,20 @@ const Register = () => {
   return (
     <div className='flex flex-1 h-screen'>
       <div className='relative hidden w-0 flex-1 lg:block'>
-        <img
-          className='absolute inset-0 h-full w-full object-cover'
-          src={registerPhoto}
-          alt='Logo'
-        />
+        <Link to='/'>
+          <img
+            className='absolute inset-0 h-full w-full object-cover'
+            src={registerPhoto}
+            alt='Logo'
+          />
+        </Link>
       </div>
       <div className='flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24'>
         <div className='mx-auto w-full max-w-sm lg:w-96'>
           <div>
-            <img className='h-10 w-auto' src='' alt='Logo' />
+            <Link to='/'>
+              <img className='h-10 w-auto' src='' alt='Logo' />
+            </Link>
             <h2 className='mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900 uppercase'>
               REGISTRO DE USUARIO
             </h2>
@@ -208,6 +212,12 @@ const Register = () => {
               </div>
             </div>
           </div>
+          <Link
+            to='/'
+            className='text-[#27C277] font-semibold block mt-5 text-center'
+          >
+            Volver al home
+          </Link>
         </div>
       </div>
     </div>
