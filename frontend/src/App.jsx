54dde0557cd2be1c2cff57fe8d2f7d886907f37d
GloriaRamosM <gloriaramosmoran@gmail.com>
2024-03-03 19:24:26 -0300

    feat(frontend): sidebar created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,19 @@
-// import { RouterProvider } from 'react-router-dom';
-// import { Provider } from 'react-redux';
-// import router from './router.jsx';
-// import store from './store/store.js';
-// import { QueryClient, QueryClientProvider } from 'react-query';
+import { RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import router from './router.jsx';
+import store from './store/store.js';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
-import SideBarAdmin from './components/SideBarAdmin/SideBarAdmin.jsx';
-
-// // Create a client
-// const queryClient = new QueryClient();
+// Create a client
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <SideBarAdmin />
-    // <QueryClientProvider client={queryClient}>
-    //   <Provider store={store}>
-    //     <RouterProvider router={router} />
-    //   </Provider>
-    // </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </QueryClientProvider>
   );
 }
 export default App;
