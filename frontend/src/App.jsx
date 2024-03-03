55cd892449c36dcf0de4bb9213fe31f7f51c7b59
GloriaRamosM <gloriaramosmoran@gmail.com>
2024-03-03 19:17:27 -0300

    feat(frontend): sidebar from admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,22 @@
-import { RouterProvider } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import router from './router.jsx';
-import store from './store/store.js';
-import { QueryClient, QueryClientProvider } from 'react-query';
+// import { RouterProvider } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+// import router from './router.jsx';
+// import store from './store/store.js';
+// import { QueryClient, QueryClientProvider } from 'react-query';
 
-// Create a client
-const queryClient = new QueryClient();
+import SideBarAdmin from './components/SideBarAdmin/SideBarAdmin.jsx';
+
+// // Create a client
+// const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <RouterProvider router={router} />
-      </Provider>
-    </QueryClientProvider>
+    <SideBarAdmin />
+    // <QueryClientProvider client={queryClient}>
+    //   <Provider store={store}>
+    //     <RouterProvider router={router} />
+    //   </Provider>
+    // </QueryClientProvider>
   );
 }
 export default App;
