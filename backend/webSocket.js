0e525eeae14dd13d79887ccc2af79a90e4edf6ea
Jhordy Gavinchu <dev@jhordyess.com>
2024-03-04 14:52:10 +0000

    fix(backend): updated and integrated websockets with services

--- backend/webSocket.js
+++ backend/webSocket.js
@@ -1,15 +1,37 @@
 const { parse } = require('node:path');
 const { WebSocketServer, OPEN } = require('ws');
+const {
+  getSeatsByScheduleId,
+  toggleSeatState,
+  checkScheduleExistsById
+} = require('./services/webSocket');
 
-const WS_DIR = '/ws/v1';
-const WS_BASE = 'seats';
+const WS_DIR = '/ws/v1/seats/schedule';
 
 const wsServer = new WebSocketServer({ noServer: true });
 
 wsServer.on('error', console.error);
 
-wsServer.on('connection', (ws, req) => {
+wsServer.on('connection', async (ws, req) => {
   const address = req.socket.remoteAddress;
+  const { base } = parse(req.url);
+
+  const scheduleId = Number(base);
+
+  const scheduleIdExists = await checkScheduleExistsById(scheduleId);
+
+  if (!scheduleIdExists) {
+    ws.close();
+    return;
+  } else {
+    ws.send(
+      JSON.stringify({
+        status: 'success',
+        message: 'Conexión establecida, y se han enviado los asientos!',
+        body: { seats: await getSeatsByScheduleId(scheduleId) }
+      })
+    );
+  }
 
   console.log('Client connected: ', address);
 
@@ -19,52 +41,28 @@ wsServer.on('connection', (ws, req) => {
     console.log('Client disconnected: ', address);
   });
 
-  //TODO Check the request and manage a "space" that group by related clients and response with the seats
-  ws.send(
-    JSON.stringify({
-      status: 'success',
-      message: 'Conexión establecida, y se han enviado los asientos!',
-      body: {
-        total: 10,
-        seats: [
-          { id: 1, status: 'disponible' },
-          { id: 2, status: 'reservado' },
-          { id: 3, status: 'reservado' },
-          { id: 4, status: 'reservado' },
-          { id: 5, status: 'disponible' },
-          { id: 6, status: 'reservado' },
-          { id: 7, status: 'disponible' },
-          { id: 8, status: 'reservado' },
-          { id: 9, status: 'reservado' },
-          { id: 10, status: 'reservado' }
-        ]
-      }
-    })
-  );
+  ws.on('message', async (data) => {
+    const { scheduleId: scheduleIdClient, seatId, type } = JSON.parse(data);
 
-  ws.on('message', (data) => {
-    wsServer.clients.forEach((client) => {
-      // TODO Check the request and manage a "space" that group by related clients and response with the seats
-      if (client.readyState === OPEN) {
+    if (!scheduleIdClient || !seatId || !type) {
+      ws.send('Invalid data');
+      return;
+    }
+
+    if (type === 'lock') {
+      await toggleSeatState(scheduleIdClient, seatId);
+    }
+
+    const seats = await getSeatsByScheduleId(scheduleIdClient);
+
+    wsServer.clients.forEach(async (client) => {
+      //TODO prevent to send to the same client
+      if (client.readyState === OPEN && scheduleIdClient === scheduleId) {
         client.send(
           JSON.stringify({
             status: 'update',
             message: 'Asientos actualizados!',
-            body: {
-              total: 10,
-              seats: [
-                { id: 1, status: 'disponible' },
-                { id: 2, status: 'reservado' },
-                { id: 3, status: 'reservado' },
-                { id: 4, status: 'reservado' },
-                { id: 5, status: 'bloqueado' },
-                { id: 6, status: 'reservado' },
-                { id: 7, status: 'bloqueado' },
-                { id: 8, status: 'reservado' },
-                { id: 9, status: 'reservado' },
-                { id: 10, status: 'reservado' }
-              ]
-            }
+            body: { seats }
           })
         );
       }
@@ -78,8 +76,8 @@ wsServer.on('connection', (ws, req) => {
  */
 
 function upgradeHandler(req, socket, head) {
-  const { base, dir } = parse(req.url);
-  if (base === WS_BASE && dir === WS_DIR) {
+  const { dir } = parse(req.url);
+  if (dir === WS_DIR) {
     //TODO add authentication, reference: https://www.npmjs.com/package/ws#client-authentication
     wsServer.handleUpgrade(req, socket, head, (socket) => {
       wsServer.emit('connection', socket, req);
