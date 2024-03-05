8c5f37e9c0f58f1ba4f5c4a04e588c2056b82a7d
CamiloProg <camilom200107@gmail.com>
2024-03-05 12:18:42 -0500

    feat(frontend): useHook for create Routes

--- frontend/src/hooks/useRoutes.js
+++ frontend/src/hooks/useRoutes.js
@@ -1,15 +1,36 @@
-import { useQuery } from 'react-query';
-import { fetchRoutes } from '../services/Routes';
+import { useQuery, useMutation } from 'react-query';
+import { fetchRoutes, createRoute } from '../services/routes';
 
 export function useRoutes() {
-  return useQuery(['routes'], () => fetchRoutes(), {
-    // example, onSuccess you can do something
+  const { data, error, isLoading } = useQuery(['routes'], () => fetchRoutes(), {
+    // Ejemplo: en onSuccess puedes hacer algo
     onSuccess: (data) => {
       console.log(data);
     },
-    // example, onError you can do something
+    // Ejemplo: en onError puedes hacer algo
     onError: (error) => {
       return error;
     }
   });
+
+  const createNewRoute = useMutation(
+    (newRouteData) => createRoute(newRouteData),
+    {
+      onSuccess: (data) => {
+        console.log('Nueva ruta creada:', data);
+        // Aquí puedes realizar alguna acción adicional si la creación es exitosa, como refrescar la lista de rutas, etc.
+      },
+      onError: (error) => {
+        console.error('Error al crear la ruta:', error);
+        // Aquí puedes manejar el error de la creación de la ruta
+      }
+    }
+  );
+
+  return {
+    routes: data,
+    error,
+    isLoading,
+    createNewRoute: createNewRoute.mutate // Aquí exponemos la función para crear una nueva ruta
+  };
 }
