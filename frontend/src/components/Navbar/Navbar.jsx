8a58301ebdaea4c5f6470231b9d0e114a55edf0d
Emerson Suarez <108478905+Jerick97@users.noreply.github.com>
2024-03-07 13:18:06 -0500

    style(frontend): Se cambio estilos del Navbar y Logo en Footer (#253)

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Logo from './Logo';
 import { NavigationLinks } from './NavigationLinks';
 import { UserActions } from './UserActions';
@@ -8,24 +8,43 @@ import './Navbar.css';
 
 export const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+      if (windowWidth > 640 && isMenuOpen) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [windowWidth, isMenuOpen]);
+
   return (
     // TODO: el boton Mi Cuenta se rompe de 768px a 910px, solucionar
-    <nav className='bg-[#486284] md:bg-[#D3DCE7] h-16 relative md:min-h-28 border-0 md:border-2 md:border-[#697f9c] text-nowrap'>
+    <nav className='bg-[#D8ECF6] h-16 relative md:min-h-28 border-0 md:border-2 md:border-[#697f9c] text-nowrap'>
       <div className='h-full mx-6 md:mx-14 flex items-center'>
         <Logo />
-        <button
-          className={`md:hidden text-white md:text-[#486284]`}
-          onClick={toggleMenu}
-        >
-          <img src={hamburgerBar} />
-        </button>
-        <button className='md:hidden ml-4 align-middle'>
-          <img src={person} className='' />
-        </button>
+        <div className='flex items-center md:block absolute right-5'>
+          <button
+            className={`md:hidden text-white md:text-[#486284]`}
+            onClick={toggleMenu}
+          >
+            <img src={hamburgerBar} />
+          </button>
+          <button className='md:hidden ml-4 align-middle'>
+            <img src={person} className='' />
+          </button>
+        </div>
         <div
           id='navbar-items'
           className={`md:flex ${isMenuOpen ? 'flex' : 'hidden'} md:items-center md:justify-end flex-col md:flex-row overflow-hidden flex-shrink flex-grow flex-`}
@@ -37,7 +56,7 @@ export const Navbar = () => {
               <UserActions />
             </>
           ) : (
-            <ul className='absolute p-2 right-4 bg-white border rounded shadow-md'>
+            <ul className='absolute md:bottom-7 -bottom-60  p-2 right-0 z-50 md:bg-transparent md:border-none md:rounded-none md:shadow-none bg-[#8DDBB5] border-[#8DDBB5] border rounded shadow-md'>
               <NavigationLinks />
             </ul>
           )}
--- frontend/src/components/Navbar/NavigationLinks.jsx
+++ frontend/src/components/Navbar/NavigationLinks.jsx
@@ -10,17 +10,21 @@ export const NavigationLinks = () => {
   ];
 
   return (
-    <div className='flex flex-col md:flex-row gap-4 md:gap-6 items-center'>
+    <div className='flex flex-col md:flex-row gap-4 md:gap-6 items-center  md:w-auto w-40'>
       {links.map((link, index) =>
         link.label === 'Destinos' ? (
-          <Link key={index} to={link.url} className='font-semibold navbar-text'>
+          <Link
+            key={index}
+            to={link.url}
+            className='font-semibold underline navbar-text text-base md:text-lg'
+          >
             {link.label}
           </Link>
         ) : (
           <Link
             key={index}
             to={link.url}
-            className='text-[#566e8d] navbar-text'
+            className='md:text-[#566e8d] text-white navbar-text text-base md:text-lg'
           >
             {link.label}
           </Link>
@@ -28,7 +32,7 @@ export const NavigationLinks = () => {
       )}
       <Link
         to='/register'
-        className='hover:underline navbar-text text-[#566e8d]'
+        className='hover:underline navbar-text md:text-[#566e8d] text-white text-base md:text-lg'
       >
         Regístrame
       </Link>
